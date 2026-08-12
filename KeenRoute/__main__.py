9fe6be31e43b2958bf6a242171ef5375ac87fcf6
♫♣ĜaƁrĺSăTăNÏ♣♫ <55460474+TIIGR@users.noreply.github.com>
2026-08-12 18:26:13 +0300

    refresh

--- KeenRoute/__main__.py
+++ KeenRoute/__main__.py
@@ -1,4 +1,4 @@
-import KeenRoute.ndms; import sys; sys.path.append('.')
+import sys; sys.path.append('.'); import KeenRoute.ndms as Keenetic
 from KeenRoute.uploader import update
 from getpass import getpass
 
@@ -7,6 +7,6 @@ if __name__ == "__main__":
 
     pswd = str(getpass("Введите пароль от SSH: "))
 
-    update(KeenRoute.ndms.SSHClient("192.168.1.1", 222, "root", pswd, "Entware"))
-    update(KeenRoute.ndms.SSHClient("192.168.10.1", 222, "root", pswd, "Entware"))
-    update(KeenRoute.ndms.SSHClient("192.168.2.1", 22, "admin", pswd, "NDMS"))
+    update(Keenetic.SSHClient("192.168.1.1", 222, "root", pswd, "Entware"))
+    update(Keenetic.SSHClient("192.168.10.1", 222, "root", pswd, "Entware"))
+    update(Keenetic.SSHClient("192.168.2.1", 22, "admin", pswd, "NDMS"))
